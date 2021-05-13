8489e2d338b7154399b3b75715d540883b630598
Guillaume Seguin <guillaume@paralint.com>
2021-05-13 10:39:31 -0400

    Added progress bar. The tqdm library is now mandatory

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import random
 import sys
+import time
 
-try:
-    import tqdm
-    progress = tqdm.tqdm
-except ModuleNotFoundError:
-    progress = lambda x: x
+import tqdm
 
+    
 try:
     n = int(sys.argv[1])
 except:
     n = 25
 
-l1 = list(range(n))
+def wrap_progress_with_random(progress_func, rand_func):
+    def inner_random():
+        progress_func.update()
+        return rand_func()
 
-random.shuffle(l1)
+    return inner_random
 
-for l,r in enumerate(progress(l1)):
-    print(f"{l},{r}")
+print("Allocating memory...", file=sys.stderr)
+
+table = list(range(n))
+
+progress = tqdm.tqdm(total=len(table))
+
+progress_random = wrap_progress_with_random(progress, random.random)
+
+print("Suffling the table...", file=sys.stderr)
+random.shuffle(table, progress_random)
+
+progress.update()
+progress.close()
+
+print("Printing the table...", file=sys.stderr)
+for r in tqdm.tqdm(table):
+    print(r)
 
